06e51d89fce35ef083ecb6321a35eda3c3179bd9
Rishi Kumar Reddy <53998053+rishikumar-p@users.noreply.github.com>
2023-12-05 20:06:43 -0700

    Update app.py

--- mood_recognition_server/app.py
+++ mood_recognition_server/app.py
@@ -4,6 +4,7 @@ from werkzeug.utils import secure_filename
 from predictEmotion import getEmotion
 from predictDistraction import getDistraction
 from predictWorkload import getWorkload
+from firebaseDB import create_or_update_record
 import json
 
 app = Flask(__name__)
@@ -18,15 +19,19 @@ def hello_world():
 def emotion():
     print(request.files)
     try:
-        if 'file' not in request.files:
+        if 'image' not in request.files:
             return jsonify({'error': 'file not provided'}), 400
-        file = request.files['file']
+        if 'id' not in request.form:
+            return jsonify({'error': 'id not provided'}), 400
+        file = request.files['image']
+        id = request.form['id']
         if file.filename =='':
             return jsonify({'error': 'no file selected'}), 400 
         if file:
             filename = secure_filename(file.filename)
             result = getEmotion(file)
-            return jsonify({'prediction': result}), 200
+            create_or_update_record(id, {"UUID": id, "emotion": result} )
+            return jsonify({'prediction': result, 'id': id}), 200
     except:
         return jsonify({'error': 'something went wrong'}), 500
     
@@ -34,30 +39,34 @@ def emotion():
 @app.route('/distraction', methods=['POST'])
 def distraction():
     try:
-        if 'file' not in request.files:
+        if 'image' not in request.files:
             return jsonify({'error': 'file not provided'}), 400
-        file = request.files['file']
+        file = request.files['image']
+        id = request.form['id']
         if file.filename =='':
             return jsonify({'error': 'no file selected'}), 400
         if file:
             filename = secure_filename(file.filename)
             result = getDistraction(file)
-            return jsonify({'prediction': result}), 200
+            create_or_update_record(id, {"UUID": id, "distraction": result} )
+            return jsonify({'id': id, 'prediction': result}), 200
     except:
         return jsonify({'error': 'something went wrong'}), 500
 
 @app.route('/workload', methods=['POST'])
 def workload():
     try:
-        if 'file' not in request.files:
+        if 'image' not in request.files:
             return jsonify({'error': 'file not provided'}), 400
-        file = request.files['file']
+        file = request.files['image']
+        id = request.form['id']
         if file.filename =='':
             return jsonify({'error': 'no file selected'}), 400
         if file:
             filename = secure_filename(file.filename)
             result = getWorkload(file)
-            return jsonify({'prediction': result}), 200
+            create_or_update_record(id, {"UUID": id, "workload": result} )
+            return jsonify({'id': id,'prediction': result}), 200
     except:
         return jsonify({'error': 'something went wrong'}), 500
 
