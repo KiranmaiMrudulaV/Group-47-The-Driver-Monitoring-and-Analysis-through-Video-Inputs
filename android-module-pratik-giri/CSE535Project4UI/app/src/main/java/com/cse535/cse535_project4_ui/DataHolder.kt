6c0b1e37191969217ab2f9fbf0afcee079abfdd5
Manjju Shree <52458602+manju1201@users.noreply.github.com>
2023-12-07 08:59:40 +0530

    Delete android-module-pratik-giri directory

--- android-module-pratik-giri/CSE535Project4UI/app/src/main/java/com/cse535/cse535_project4_ui/DataHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.cse535.cse535_project4_ui
-
-import androidx.lifecycle.MutableLiveData
-
-object DataHolder {
-    val responseMap: MutableLiveData<HashMap<String, MutableList<String>>> = MutableLiveData()
-
-}
--- android-module-pratik-giri/CSE535Project4UI/app/src/main/java/com/cse535/cse535_project4_ui/FirebaseRecordClass.kt
+++
@@ -1,8 +0,0 @@
-package com.cse535.cse535_project4_ui
-
-data class FirebaseRecordClass(
-    val UUID: String? = null,
-    val distraction: String? = null,
-    val emotion: String? = null,
-    val workload: String? = null
-)
